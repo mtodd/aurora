31b10cd3f96265a7e24b4ba3acf35866571875b9
mtodd <mtodd@9f4137a4-b70c-4f5a-9185-1fc392975a94>
2008-01-23 00:43:50 +0000

    Fixed problems with the authenticate method, removing duplication and making it an instance method.
    git-svn-id: svn+ssh://rubyforge.org/var/svn/aurora/trunk@15 9f4137a4-b70c-4f5a-9185-1fc392975a94

--- lib/aurora/client.rb
+++ lib/aurora/client.rb
@@ -35,9 +35,8 @@ module Aurora
     # Expects a Hash with either a <tt>:username</tt> and <tt>:password</tt>
     # pair or a single <tt>:token</tt> option. The server authentication
     # method is called accordingly.
-    def self.authenticate(options = {})
+    def authenticate(options = {})
       if user, pass = options[:username], options[:password]
-        user, pass = options[:username], options[:password]
         post("/user/auth/#{user}", :password => pass)[:body] rescue false
       elsif token = options[:token]
         get("/token/auth/#{token}") rescue false 
